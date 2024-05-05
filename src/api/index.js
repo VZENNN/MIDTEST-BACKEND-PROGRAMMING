39fe9dd97b814bf6c7310192c8bbd4cfa07d1f8b
VZENNN <viezenp@gmail.com>
2024-05-05 18:36:59 +0700

    FINAL COMMIT

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const authentication = require('./components/authentication/authentication-route');
 const users = require('./components/users/users-route');
+const olshop = require('./components/olshop/olshop-route');
+const purchase = require('./components/purchase/purchase-route');
 
 module.exports = () => {
   const app = express.Router();
 
   authentication(app);
   users(app);
+  olshop(app);
+  purchase(app);
 
   return app;
 };
--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,9 @@ const config = require('../core/config');
 const logger = require('../core/logger')('app');
 
 const usersSchema = require('./users-schema');
+const olshopSchema = require('./olshop-schema');
+const purchaseSchema = require('./purchase-schema');
+const timestamp = require('./olshop-schema');
 
 mongoose.connect(`${config.database.connection}/${config.database.name}`, {
   useNewUrlParser: true,
@@ -14,8 +17,12 @@ db.once('open', () => {
 });
 
 const User = mongoose.model('users', mongoose.Schema(usersSchema));
+const Olshop = mongoose.model('barang', mongoose.Schema(olshopSchema));
+const Purchase = mongoose.model('purchase', mongoose.Schema(purchaseSchema));
 
 module.exports = {
   mongoose,
   User,
+  Olshop,
+  Purchase,
 };
